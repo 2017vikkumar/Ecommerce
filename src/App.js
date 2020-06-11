929b401de5e7aef22fa23248f93cbef46c807957
Vikash Kumar <vikkumar@iu.edu>
2020-06-10 23:49:20 -0400

    V1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.jsx';
+
+function App() {
+  return (
+    <HomePage></HomePage>
+  );
+}
+
+export default App;
